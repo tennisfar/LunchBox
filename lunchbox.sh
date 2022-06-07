58a395ed42e0ee18c6a17e42e0da55d92ee0b75f
Michael L Petersen <ekmlpe@danskespil.dk>
2022-06-07 10:01:20 +0200

    brs updates

--- lunchbox.sh
+++ lunchbox.sh
@@ -110,6 +110,7 @@ alias compare='ds && .. && cd ds-compare-screenshots && npm start && cd ./output
 
 # merge latest updates from pam-n-sub into current branch
 alias gitpam='git fetch && git merge origin/pam-n-sub'
+alias gittc='git fetch && git merge origin/master'
 
 # merge latest updates from specific release branch into current branch
 alias gitrel='gitRel'
