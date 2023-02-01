f089bf9e362894cf6d2c608d328997ca1fec14f3
Michael L Petersen <ekmlpe@danskespil.dk>
2023-02-01 10:39:40 +0100

    :package:

--- lunchbox.sh
+++ lunchbox.sh
@@ -146,7 +146,7 @@ gitCompareWith() {
   git log --oneline --no-merges --author=ekmlpe origin/$1..$2
 }
 
-alias bygds='ds; rm Website/obj -r ; cd - ; echo Removed obj folder'
+alias bygds='ds; rm Website/obj -r ; cd - > null ; echo Removed obj folder'
 
 
 # Hours
