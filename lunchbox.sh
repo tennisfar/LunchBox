16de8420cec8407990780a7b482c628f4acd6f66
Michael L Petersen <ekmlpe@danskespil.dk>
2025-04-02 12:58:35 +0200

    :package:

--- lunchbox.sh
+++ lunchbox.sh
@@ -192,7 +192,7 @@ gitreleases() {
 alias latestrel='gitreleases'
 alias w='gulp watch'
 alias g='gulp'
-alias gw='gulp && gulp watch'
+alias gw='gulp && npm run build:v2 && gulp watch'
 alias gw2='gulp && npm run build:v2 && gulp watch'
 alias gl='gulp lint && gulp stylelint'
 alias gw-bingo='gulp && gulp watch --theme Bingo'
